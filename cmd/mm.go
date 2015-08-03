23b08fc39e7e099fcf43b0910f9e285df06d23b5
aoeu <travis@mcdem.us>
2015-08-03 09:09:20 -0400

    Add commands to dump audio card and monitor MIDI

---
+++ cmd/mm.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"audio/midi"
+	"flag"
+	"fmt"
+	"log"
+)
+
+func main() {
+	deviceName := flag.String("device", "nanoPAD2 MIDI 1", "The MIDI device name to monitor.")
+	flag.Parse()
+	devices, err := midi.GetDevices()
+	if err != nil {
+		log.Fatal(err)
+	}
+	device, ok := devices[*deviceName]
+	if !ok {
+		log.Fatal("No device with name '" + *deviceName + "' found.\n")
+	}
+	if err := device.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := device.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	go device.Run()
+	for {
+		e := <-device.OutPort().Events()
+		fmt.Printf("%+v\n", e)
+	}
+}
